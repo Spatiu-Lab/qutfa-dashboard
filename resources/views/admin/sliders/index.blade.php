cad1219cba0280e2e455e80943d06251518ceb9e
Moez Sabri <45544600+Almandeel@users.noreply.github.com>
2022-08-17 19:43:26 +0200

    add sliders

---
+++ resources/views/admin/sliders/index.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.admin')
+@section('content')
+<div class="col-12 p-3">
+	<div class="col-12 col-lg-12 p-0 main-box">
+		<div class="col-12 px-0">
+			<div class="col-12 p-0 row">
+				<div class="col-12 col-lg-4 py-3 px-3">
+					<span class="fas fa-articles"></span> المعرض
+				</div>
+				<div class="col-12 col-lg-4 p-2">
+				</div>
+				<div class="col-12 col-lg-4 p-2 text-lg-end">
+					@can('create',\App\Models\Slider::class)
+					<a href="{{route('admin.sliders.create')}}">
+						<span class="btn btn-primary"><span class="fas fa-plus"></span> إضافة جديد</span>
+					</a>
+					@endcan
+				</div>
+			</div>
+			<div class="col-12 divider" style="min-height: 2px;"></div>
+		</div>
+
+		<div class="col-12 py-2 px-2 row">
+			<div class="col-12 col-lg-4 p-2">
+				<form method="GET">
+					<input type="text" name="q" class="form-control" placeholder="بحث ... ">
+				</form>
+			</div>
+		</div>
+		<div class="col-12 p-3" style="overflow:auto">
+			<div class="col-12 p-0" style="min-width:1100px;">
+				
+			
+			<table class="table table-bordered  table-hover">
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>العنوان</th>
+						<th>تحكم</th>
+					</tr>
+				</thead>
+				<tbody>
+					@foreach($sliders as $slider)
+					<tr>
+						<td>{{$slider->id}}</td>
+						<td>{{$slider->title}}</td>
+						<td style="width: 270px;">
+
+							@can('update',$slider)
+							<a href="{{route('admin.sliders.edit',$slider)}}">
+								<span class="btn  btn-outline-success btn-sm font-1 mx-1">
+									<span class="fas fa-wrench "></span> تحكم
+								</span>
+							</a>
+							@endcan
+							@can('delete',$slider)
+							<form method="POST" action="{{route('admin.sliders.destroy',$slider)}}" class="d-inline-block">@csrf @method("DELETE")
+								<button class="btn  btn-outline-danger btn-sm font-1 mx-1" onclick="var result = confirm('هل أنت متأكد من عملية الحذف ؟');if(result){}else{event.preventDefault()}">
+									<span class="fas fa-trash "></span> حذف
+								</button>
+							</form>
+							@endcan
+						</td>
+					</tr>
+					@endforeach
+				</tbody>
+			</table>
+			</div>
+		</div>
+		<div class="col-12 p-3">
+			{{$sliders->appends(request()->query())->render()}}
+		</div>
+	</div>
+</div>
+@endsection
