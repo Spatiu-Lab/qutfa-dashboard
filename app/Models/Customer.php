db9b87c774e23adb57312a044b7e6f17b3a43cca
Moez Sabri <45544600+Almandeel@users.noreply.github.com>
2022-09-04 22:56:20 +0200

    wip

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Customer extends Model
 {
@@ -15,4 +16,9 @@ class Customer extends Model
         'email',
         'address',
     ];
+
+    public function user() : HasOne
+    {
+        return $this->hasOne(User::class, 'customer_id');
+    }
 }
